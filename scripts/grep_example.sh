6fc720464c98207ee81a13c99c5b823aee90d065
Maximiliano Greco <mgreco@ets.es>
2019-04-30 18:57:33 +0200

    update

--- scripts/grep_example.sh
+++ scripts/grep_example.sh
@@ -1,1 +1,2 @@
 grep -lr "JUMP=72" ./ | xargs grep -l "SCORE_TEST=0.06" | xargs grep -r "COMMAND"
+grep -r -l -E "SCORE_TEST=0\.0[1][1-2][0-2]" | xargs grep -r "SCORE_TEST"
---
+++ scripts/large_amount_files_parallel_regex.sh
@@ -0,0 +1,1 @@
+find . -regextype posix-extended -regex ".*0\.02[2-9]+.h5" | parallel rm
