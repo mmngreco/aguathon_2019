36187bbee46f95ce0864bb91e8e289e96a05ab18
Maximiliano Greco <mgreco@ets.es>
2019-04-30 19:13:56 +0200

    update py

--- print_grid_commands.py
+++ print_grid_commands.py
@@ -6,20 +6,21 @@ def grid_search():
     seed = 7
     np.random.seed(seed)
     params = {
-        "-b": [900],
-        "-f": [*range(70, 200, 8)],
-        "--l1l2": ["'l2(0)'", "'l2(0.01)'", "'l2(0.02)'"],
-        "-n1": [250, 488],
-        "-n2": [*range(0, 32, 2)],
+        "-b": [*range(200, 8000, 200)],
+        "-f": [*range(60, 300, 8)],
+        "--l1l2": ["'l2(0)'", "'l2(0.001)'", "'l2(0.005)'"],
+        "-n1": [*range(6, 2048, 64)],
+        "-n2": [0],
         "-e": [40],
-        "-p": [6],
+        "-p": [3, 6, 9, 12],
         "-la": [72]
      }
 
     params_collection = list(ParameterGrid(params))
     np.random.shuffle(params_collection)
+    N = 100
 
-    for params in params_collection:
+    for params in params_collection[:N]:
         args = " ".join([f"{k} {v}"for k, v in params.items()])
         print(f"\"python CNN_feat2d.py {args}\"")
 
